1fce382911d1dc076084079590536d1947a6f2b1
Alexey Ryumin <aryumin@aryumin.ru>
2017-10-04 00:02:27 +0300

    tests project added

---
+++ SamplesToTextsMatcherTests/MatcherTests.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace SamplesToTextsMatcherTests
+{
+    public class MatcherTests
+    {
+        
+    }
+}
---
+++ SamplesToTextsMatcherTests/ParserTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xunit;
+
+namespace SamplesToTextsMatcherTests
+{
+    public class ParserTests
+    {
+        
+    }
+}
