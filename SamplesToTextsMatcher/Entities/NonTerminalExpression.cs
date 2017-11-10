139c273b4027a313df0ff320b8f622b49c23fdd8
Alexey Ryumin <aryumin@aryumin.ru>
2017-11-11 00:27:48 +0300

    added priority property to non terminal expression

--- SamplesToTextsMatcher/Entities/NonTerminalExpression.cs
+++ SamplesToTextsMatcher/Entities/NonTerminalExpression.cs
@@ -3,6 +3,12 @@ namespace SamplesToTextsMatcher.Entities
 {
     public abstract class NonTerminalExpression : Expression
     {
+        /// <summary>
+        /// Priority of non-terminal operation.
+        /// </summary>
+        /// <value>The priority.</value>
+        public int Priority { get; set; }
+
         public abstract override void Interpret(Context context);
     }
 }
