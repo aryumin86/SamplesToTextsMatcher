7eb1674d5808d7db57ec6622fe3ce1dd992ccc0e
Alexey Ryumin <aryumin@aryumin.ru>
2017-11-11 00:25:05 +0300

    updates

--- SamplesToTextsMatcher/Entities/TerminalExpression.cs
+++ SamplesToTextsMatcher/Entities/TerminalExpression.cs
@@ -3,10 +3,15 @@ namespace SamplesToTextsMatcher.Entities
 {
     public class TerminalExpression : Expression
     {
-        private string _symbol;
+        /// <summary>
+        /// This term shold be used in this exact form (no term forms are needed).
+        /// </summary>
+        /// <value><c>true</c> if needs exact form; otherwise, <c>false</c>.</value>
+        public bool NeedsExactForm { get; set; }
 
         public TerminalExpression(string symbol){
-            _symbol = symbol;
+            Raw = symbol;
+            NeedsExactForm = true;
         }
 
         public override void Interpret(Context context)
